8e9a058eb270d6554984576e6a4749d61d71d905
Greg Williams <github@gregwilliams.eu>
2014-07-13 19:15:25 +0100

    Initial checkin of system.py containing daemonise()

---
+++ python/mc/system.py
@@ -0,0 +1,41 @@
+import os
+import logging
+import sys
+import string
+import os.path
+
+def daemonise(arg_array):
+    """Daemonise a new process (fork/exec + re-parent to init)
+
+    """
+    executable = arg_array[0]
+    if not os.path.isfile(executable):
+        raise Exception("Executable \"%s\" does not exist" % executable)
+
+    #TODO - use a "with" statement to destruct pipe?
+    pipe_r, pipe_w = os.pipe()
+    child_pid = os.fork()
+    if child_pid == 0:
+        #In child
+        os.close(pipe_r)
+        pid = os.fork()
+        if pid == 0:
+            #In grandchild
+            try:
+                os.execvp(arg_array[0], arg_array)
+            except OSError:
+                logging.error("Unable to exec command: %s" % (string.join(arg_array, " ")))
+                os._exit(1)
+        #Still in 1st child
+        pipe_w = os.fdopen(pipe_w, 'w')
+        pipe_w.write("%d" % pid)
+        pipe_w.close()
+        os._exit(0)
+    #In parent
+    os.close(pipe_w)
+    pipe_r = os.fdopen(pipe_r, 'r')
+    pid = int(pipe_r.read())
+    pipe_r.close()
+    os.waitpid(child_pid, 0)
+    logging.info("Daemonised new process (pid=%d)" % pid)        
+    return pid
---
+++ python/mc/tests/test_system.py
@@ -0,0 +1,27 @@
+
+import mc.system
+import tempfile
+import unittest
+import os.path
+import shutil
+
+class TestSystem(unittest.TestCase):
+    def test_daemonise_success(self):
+        dd = tempfile.mkdtemp()
+        ff = dd + "/foo"
+        cmd = [ '/usr/bin/touch', ff ]
+        pid = mc.system.daemonise(cmd)
+        
+        #Wait for it to die
+        try:
+            while os.kill(pid, 0):
+                pass
+        except OSError:
+            pass
+
+        self.assertTrue(os.path.isfile(ff))
+
+        shutil.rmtree(dd)
+
+    def test_daemonise_invalid_command(self):
+        self.assertRaisesRegexp(Exception, "does not exist", mc.system.daemonise, ['/sdfsd/fs/df/dsg/sdg'])
